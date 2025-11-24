7afed391a6a3da90de9ea2baa4f842b989e6c6b0
bobdillon <alxschaub@gmail.com>
2025-11-24 15:48:31 -0600

    fixes applied

--- geminicall.py
+++ geminicall.py
@@ -1,6 +1,8 @@
 import google.generativeai as genai
+import os
 
-genai.configure(api_key="AIzaSyCzJdDbv8bgHedR3wHauggh2u54Fyo7AGA")
+api_key = os.getenv("gemini_api_key")
+genai.configure(api_key=api_key)
 model = genai.GenerativeModel('gemini-1.5-flash')  # or 'gemini-pro'
 
 response = model.generate_content("Hello, how are you, lmk in emojis?")
